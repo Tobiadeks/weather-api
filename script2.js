c8965cb5a6dffc5fb886b46b4cd4de25a934734c
Tobiadeks <tobiadekunle00@gmail.com>
2022-12-08 14:22:00 +0100

    quote api

--- script2.js
+++ script2.js
@@ -1,9 +1,11 @@
+var category = $("#category").val()
 $(document).ready(function () {
+    var a = Math.random()*10
     $("#quotebutton").click(()=>{
-        var url = `https://zenquotes.io/api/quotes/[your_key]`
-        fetch(url, {method:"GET"})
+        var url = `https://zenquotes.io/api/random `
+        fetch(url,{method:"GET",headers:{'Content-Type':'application/json'}})
         .then((response)=>response.json())
-        .then((response)=>console.log(response))
+        .then((response)=>{console.log(response)})
         .catch((err)=>console.log(err))
         
      
